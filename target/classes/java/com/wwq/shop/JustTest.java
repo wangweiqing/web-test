47bd70e02a463fdbf8b0f3e4aeae6c4fab82ecdf
wangweiqing <weiqingking@gmail.com>
2014-03-31 23:26:24 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   pom.xml
    	new file:   src/main/resources/java/com/wwq/shop/Battery.java
    	new file:   src/main/resources/java/com/wwq/shop/Disc.java
    	new file:   src/main/resources/java/com/wwq/shop/JustTest.java
    	new file:   src/main/resources/java/com/wwq/shop/Product.java
    	new file:   src/main/resources/java/com/wwq/shop/ProductCreator.java
    	new file:   src/main/resources/java/com/wwq/shop/SequenceGenerator.java
            new file:   src/main/webapp/WEB-INF/applicationContext.xml
    	new file:   src/main/webapp/WEB-INF/web.xml
    	new file:   src/main/webapp/index.jsp
    	new file:   target/classes/com/wwq/shop/Battery.class
    	new file:   target/classes/com/wwq/shop/Disc.class
    	new file:   target/classes/com/wwq/shop/JustTest.class
    	new file:   target/classes/com/wwq/shop/Product.class
    	new file:   target/classes/com/wwq/shop/ProductCreator.class
    	new file:   target/classes/com/wwq/shop/SequenceGenerator.class
    	new file:   target/classes/java/com/wwq/shop/Battery.java
    	new file:   target/classes/java/com/wwq/shop/Disc.java
    	new file:   target/classes/java/com/wwq/shop/JustTest.java
    	new file:   target/classes/java/com/wwq/shop/Product.java
    	new file:   target/classes/java/com/wwq/shop/ProductCreator.java
    	new file:   target/classes/java/com/wwq/shop/SequenceGenerator.java
    	new file:   target/m2e-jee/web-resources/META-INF/MANIFEST.MF
    	new file:   target/m2e-jee/web-resources/META-INF/maven/com.wwq.test/maven-test/pom.properties
    	new file:   target/m2e-jee/web-resources/META-INF/maven/com.wwq.test/maven-test/pom.xml

---
+++ target/classes/java/com/wwq/shop/JustTest.java
@@ -0,0 +1,12 @@
+package com.wwq.shop;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("applicationContext.xml")
+public class JustTest{
+	
+}
